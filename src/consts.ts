6c36ab413384052915d8a6087f26579ef7bd9c96
embuildit <contact.of.pulpul@gmail.com>
2025-03-05 22:52:17 -0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "SOC2 Certification Directory";
+export const SITE_DESCRIPTION = "A comprehensive directory of companies offering SOC 2 compliance solutions and services";
